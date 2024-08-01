de9b1f3f8f4fef1ba36c2e785251dec2ddf75e21
AmrahovaDilber <amrahovad911@gmail.com>
2024-08-01 09:19:42 +0400

    Created project

---
+++ app/_components/Logo.js
@@ -0,0 +1,12 @@
+function Logo() {
+    return (
+        <a href='/' className='flex items-center gap-4 z-10' >
+            <Image src=''>
+
+                <span  className='text-xl font-semibdol text-primary-100'>The Wild Oasis</span>
+            </Image>
+
+        </a>
+    )
+}
+export default Logo;
